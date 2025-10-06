c90694ef3d12b4c9fda87e099a9d6a52ed82dbc5
AAA <>
2025-10-06 10:56:27 +0200

    re-added metadata, it broke the site, but made it relative to the hosting path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Parkcheck",
-  description: "Check if you have parked legally."
+  description: "Check if you have parked legally.",
+  manifest: "./manifest.json"
 };
 
 export default function RootLayout({
